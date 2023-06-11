8732ce4b4ff447469a66a2a4e84509404ed03256
benjyalper <123838171+benjyalper@users.noreply.github.com>
2023-06-11 11:27:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//selecting html elements
+
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message');
+let submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', function (e) {
+	e.preventDefault();
+
+	let emailValue = emailElement.value;
+	let messageValue = messageElement.value;
+
+	if (emailValue.includes('@')) {
+		// all good
+		alert('Thank you for your message!');
+		console.log(messageValue)
+	} else {
+		alert('Oh no. That does not look like a valid email adress, please try again.');
+	}
+
+
+});
+
+
+//attaching 'click listener'
+//getting user entered values
+//javascript validations
