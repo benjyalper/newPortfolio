0b1ed3e9938fb3747fa89649afb2239c966ba6f1
benjyalper <benjyalper@hotmail.com>
2023-06-11 14:27:26 +0300

    js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 //selecting html elements
 
@@ -26,33 +26,3 @@ submitButton.addEventListener('click', function (e) {
 
 //attaching 'click listener'
 //getting user entered values
-=======
-
-//selecting html elements
-
-let emailElement = document.querySelector('#email');
-let messageElement = document.querySelector('#message');
-let submitButton = document.querySelector('#submit-button');
-
-submitButton.addEventListener('click', function (e) {
-	e.preventDefault();
-
-	let emailValue = emailElement.value;
-	let messageValue = messageElement.value;
-
-	if (emailValue.includes('@')) {
-		// all good
-		alert('Thank you for your message!');
-		console.log(messageValue)
-	} else {
-		alert('Oh no. That does not look like a valid email adress, please try again.');
-	}
-
-
-});
-
-
-//attaching 'click listener'
-//getting user entered values
->>>>>>> f2cfcdbc51ed2b3f085955da7bc5900327d91b2d
-//javascript validations
